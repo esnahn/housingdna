7467fbe0d3127c78e7af4ed0e21140773853a7f1
Euisoon Ahn <euisoon.ahn@gmail.com>
2021-11-16 12:29:08 +0900

    initial commit

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import sys
+from . import main
+
+return_code = 1  # Catchall for general errors
+try:
+    main()
+    return_code = 0
+except Exception as e:
+    print(f"Error: {e}", file=sys.stderr)
+sys.exit(return_code)
